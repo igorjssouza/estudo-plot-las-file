b1d25254a41faef9aa9f81949ba4d5e50394607f
igorjssouza <igorjs.souza@gmail.com>
2023-05-11 13:35:04 -0300

    Make some changes

--- plotLasFile.py
+++ plotLasFile.py
@@ -1,22 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backend_bases import MouseButton
 import lasio
 
-las = lasio.read("Petrobras\\15-9-19_SR_COMP.LAS")
+las = lasio.read("15-9-19_SR_COMP.LAS")
 
 df = las.df()
 df.reset_index(inplace=True)
 df.rename(columns={'DEPT':'DEPTH'}, inplace=True)
-""" df.plot(subplots=True)
-plt.show()
- """
-print(max(df['GR']))
-print()
-fig=plt.subplots(figsize=(2.8,1.6*6),layout='tight')
+
+fig=plt.subplots(figsize=(3.2,1.6*6),layout='tight')
 ax1 = plt.subplot2grid((1,1),(0,0),rowspan=1,colspan=1)
 ax1.plot("GR","DEPTH",data=df, color="green")
 ax1.set_xlabel("Gamma")
-ax1.set_xlim(min(df['GR']),max(df['GR']))
+ax1.set_xlim(min(df['GR']),max(df['GR']/2))
 ax1.set_ylim(df['DEPTH'].iloc[-1],df['DEPTH'].iloc[0])
 ax1.grid()
+
+clicks = []
+def on_click(event):
+    if event.button is MouseButton.LEFT:
+        print(f'xData {event.xdata:.2f} yData {event.ydata:.2f}')
+        clicks.append((event.xdata, event.ydata))
+        """,
+              f'pixel coords {event.x} {event.y}')"""
+
+binding_id = plt.connect('motion_notify_event', on_click)
+plt.connect('button_press_event', on_click)
+
+
 plt.show()
+with open('clicks.txt', 'w') as f:
+    for click in clicks:
+        f.write(f'{click[0]:.2f}, {click[1]:.2f}\n')
