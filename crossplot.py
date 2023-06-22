13456a6307b7f7d2093aec8e55846065f0af4241
igorjssouza <igorjs.souza@gmail.com>
2023-06-22 14:08:19 -0300

    [WIP] For 2 files or more is occur axis inconsistence

--- crossplot.py
+++ crossplot.py
@@ -15,7 +15,7 @@ class MyFrame(wx.Frame):
 
         self.log_choices = []  # Available log choices
 
-        self.button = wx.Button(self.panel, label="Select LAS File", pos=(10, 10))
+        self.button = wx.Button(self.panel, label="Select LAS Files", pos=(10, 10))
         self.button.Bind(wx.EVT_BUTTON, self.on_button_click)
 
         self.y_label = wx.StaticText(self.panel, label="Y-axis:", pos=(10, 40))
@@ -27,134 +27,140 @@ class MyFrame(wx.Frame):
         self.gr_label = wx.StaticText(self.panel, label="Color:", pos=(10, 110))
         self.gr_combo = wx.ComboBox(self.panel, pos=(70, 110), choices=self.log_choices, style=wx.CB_READONLY)
 
-        # self.button = wx.Button(self.panel, label="Select LAS File", pos=(10, 110))
-        # self.button.Bind(wx.EVT_BUTTON, self.on_button_click)
-
         self.ok_button = wx.Button(self.panel, label="OK", pos=(220, 180))
         self.ok_button.Bind(wx.EVT_BUTTON, self.on_ok_button_click)
 
         self.cancel_button = wx.Button(self.panel, label="Cancel", pos=(300, 180))
         self.cancel_button.Bind(wx.EVT_BUTTON, self.on_cancel_button_click)
 
-
         self.canvas = None
         self.ax = None
         self.polygon_selector = None
 
-        self.df = None  # Store DataFrame
+        self.dataframes = []  # Store multiple DataFrames
 
     def on_ok_button_click(self, event):
         if self.y_combo.GetValue() and self.x_combo.GetValue() and self.gr_combo.GetValue():
-            self.create_scatter_plot()
+            self.create_scatter_plots()
 
     def on_cancel_button_click(self, event):
         self.Close()
 
     def on_button_click(self, event):
-        dialog = wx.FileDialog(self, "Select LAS File", wildcard="LAS files (*.las)|*.las",
-                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        dialog = wx.FileDialog(
+            self,
+            "Select LAS Files",
+            wildcard="LAS files (*.las)|*.las",
+            style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE
+        )
         if dialog.ShowModal() == wx.ID_OK:
-            filename = dialog.GetPath()
+            filenames = dialog.GetPaths()
 
-            # Read LAS file
-            las = lasio.read(filename)
-            df = las.df()
-            df.reset_index(inplace=True)
-            # df.rename(columns={'DEPT': 'DEPTH'}, inplace=True)
+            for filename in filenames:
+                # Read LAS file
+                las = lasio.read(filename)
+                df = las.df()
+                df.reset_index(inplace=True)
 
-            self.df = df  # Store DataFrame
+                # Store DataFrame
+                self.dataframes.append(df)
 
             # Update log choices
-            self.log_choices = list(df.columns)
+            self.update_log_choices()
 
-            # Update combo boxes
-            self.y_combo.Clear()
-            self.y_combo.AppendItems(self.log_choices)
-
-            self.x_combo.Clear()
-            self.x_combo.AppendItems(self.log_choices)
-
-            self.gr_combo.Clear()
-            self.gr_combo.AppendItems(self.log_choices)
-        
         dialog.Destroy()
 
-            # Create scatter plot
-            # fig, ax = plt.subplots()
-            # ax.scatter(x='NEU', y='DEN', data=df, c=df['GR'], vmin=0, vmax=100, cmap='rainbow')
-            # ax.set_ylabel('Bulk Density (DEN) - g/cc', fontsize=14)
-            # ax.set_xlabel('Neutron Porosity (NEU) - %', fontsize=14)
-    def create_scatter_plot(self):
-        
-        fig, ax = plt.subplots()
-        scatter_plot=ax.scatter(x=self.x_combo.GetValue(), y=self.y_combo.GetValue(), data = self.df,
-                   c=self.df[self.gr_combo.GetValue()], vmin = 0, vmax=100, cmap='rainbow')
-        
-        ax.set_ylabel(f"{self.y_combo.GetValue()}", fontsize=14)
-        ax.set_xlabel(f"{self.x_combo.GetValue()}", fontsize=14)
+    def create_scatter_plots(self):
+        for df in self.dataframes:
+            fig, ax = plt.subplots()
+            scatter_plot = ax.scatter(
+                x=self.x_combo.GetValue(),
+                y=self.y_combo.GetValue(),
+                data=df,
+                c=df[self.gr_combo.GetValue()],
+                vmin=0,
+                vmax=100,
+                cmap='rainbow'
+            )
 
-        # Add a label beside the color bar
-        cbar = plt.colorbar(scatter_plot)
-        cbar.ax.set_ylabel(self.gr_combo.GetValue(), rotation=270, labelpad=15, fontsize=12)
-        
-        # Clear the previous plot, if exists
-        if self.canvas is not None:
-            self.canvas.Destroy()
+            ax.set_ylabel(f"{self.y_combo.GetValue()}", fontsize=14)
+            ax.set_xlabel(f"{self.x_combo.GetValue()}", fontsize=14)
 
-        # Remove buttons and combo boxes
+            # Add a label beside the color bar
+            cbar = plt.colorbar(scatter_plot)
+            cbar.ax.set_ylabel(self.gr_combo.GetValue(), rotation=270, labelpad=15, fontsize=12)
 
+            # Clear the previous plot, if exists
+            if self.canvas is not None:
+                self.canvas.Destroy()
 
-        # Create a wxPython panel and embed the new plot in it
-        self.canvas = wxagg.FigureCanvasWxAgg(self.panel, -1, fig)
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.canvas, 1, wx.EXPAND)
-        self.panel.SetSizer(sizer)
-        self.panel.Layout()
+            # Create a wxPython panel and embed the new plot in it
+            self.canvas = wxagg.FigureCanvasWxAgg(self.panel, -1, fig)
+            sizer = wx.BoxSizer(wx.VERTICAL)
+            sizer.Add(self.canvas, 1, wx.EXPAND)
+            self.panel.SetSizer(sizer)
+            self.panel.Layout()
 
-        # Add a colorbar
-        # colorbar = fig.colorbar(ax.collections[0])
+            # Enable polygon selector
+            self.ax = ax
+            self.polygon_selector = PolygonSelector(ax, self.on_polygon_select)
 
-        # Fit the plot within the panel
-        sizer.Fit(self)
-        self.Layout()
-
-        # Enable polygon selector
-        self.ax = ax
-        self.polygon_selector = PolygonSelector(ax, self.on_polygon_select)
-
-        # dialog.Destroy()
-        self.y_label.Destroy()
-        self.y_combo.Hide()
-        self.x_label.Destroy()
-        self.x_combo.Hide()
-        self.gr_label.Destroy()
-        self.gr_combo.Hide()
-        self.ok_button.Destroy()
-        self.cancel_button.Destroy()
-        self.button.Destroy()
+            # Remove buttons and combo boxes
+            self.y_label.Destroy()
+            self.y_combo.Hide()
+            self.x_label.Destroy()
+            self.x_combo.Hide()
+            self.gr_label.Destroy()
+            self.gr_combo.Hide()
+            self.ok_button.Destroy()
+            self.cancel_button.Destroy()
+            self.button.Destroy()
 
     def on_polygon_select(self, vertices):
         # Convert polygon vertices to a path
         path = Path(vertices)
 
-  
-        x = self.df[self.x_combo.GetValue()]
-        y = self.df[self.y_combo.GetValue()]
-        gr = self.df[self.gr_combo.GetValue()]
+        for df in self.dataframes:
+            x = df[self.x_combo.GetValue()]
+            y = df[self.y_combo.GetValue()]
+            gr = df[self.gr_combo.GetValue()]
 
-        points_inside_polygon = np.array(list(filter(path.contains_point, zip(x, y, gr))))
+            points_inside_polygon = np.array(list(filter(path.contains_point, zip(x, y, gr))))
 
-        if len(points_inside_polygon) > 0:
-            # Clear previous scatter plot
-            for collection in self.ax.collections:
-                collection.remove()
+            if len(points_inside_polygon) > 0:
+                # Clear previous scatter plot
+                for collection in self.ax.collections:
+                    collection.remove()
 
-            # Create new scatter plot with points inside the polygon
-            self.ax.scatter(x=points_inside_polygon[:, 0], y=points_inside_polygon[:, 1],
-                            c=points_inside_polygon[:, 2], vmin=0, vmax=100, cmap='rainbow')
+                # Create new scatter plot with points inside the polygon
+                self.ax.scatter(
+                    x=points_inside_polygon[:, 0],
+                    y=points_inside_polygon[:, 1],
+                    c=points_inside_polygon[:, 2],
+                    vmin=0,
+                    vmax=100,
+                    cmap='rainbow'
+                )
 
-            # Redraw the plot
-            self.canvas.draw()
+                # Redraw the plot
+                self.canvas.draw()
+
+    def update_log_choices(self):
+        self.log_choices = []
+
+        for df in self.dataframes:
+            self.log_choices.extend(list(df.columns))
+
+        # self.log_choices = list(set(self.log_choices))  # Remove duplicates
+
+        self.y_combo.Clear()
+        self.y_combo.AppendItems(self.log_choices)
+
+        self.x_combo.Clear()
+        self.x_combo.AppendItems(self.log_choices)
+
+        self.gr_combo.Clear()
+        self.gr_combo.AppendItems(self.log_choices)
 
 if __name__ == '__main__':
     app = wx.App()
